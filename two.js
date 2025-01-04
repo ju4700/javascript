dafd0acd9e149c210e84c704df2f94b8175e5b30
ju4700 <jalal.dev.design@gmail.com>
2025-01-05 01:59:16 +0600

    Add receipt display and personal information logging

---
+++ two.js
@@ -0,0 +1,11 @@
+const firstName = 'John';
+const favoriteColor = 'blue';
+
+let currentLocation = 'New York';
+let mood = 'Happy';
+
+console.log(firstName, favoriteColor, currentLocation, mood);   
+
+mood = 'Ecstatic';
+
+console.log(firstName, favoriteColor, currentLocation, mood);

14e272a5d1aceb334af7c93fecf87240d5229d06
ju4700 <jalal.dev.design@gmail.com>
2025-01-23 22:27:52 +0600

    Add initial files for Flappy Bird clone and update weather app script

---
+++ weatherscratch/script.js
@@ -0,0 +1,6 @@
+function fetchWeather() {
+    let searchInput = document.getElementById("search").value;
+    const weatherDataSection = document.getElementById("weather-data");
+    weatherDataSection.style.display = "block";
+    const apiKey = "94bbc18b34f906bac269fa611c53f3aa";
+}
